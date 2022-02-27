d0fe4a7495917168c9092ffec8af712ffcea16a6
Abdul Zagirov <zagirovaa@netcon.pro>
2022-02-27 21:24:20 +0300

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name = "mp3rename",
+    version = "0.1",
+    description = "Renaming mp3 files using random names.",
+    author = "Abdul Zagirov",
+    author_email = "zagirovaa@netcon.pro"
+)
