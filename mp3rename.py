eefcacf28a4c21f2052657a9bff31f6316553a7b
Abdul Zagirov <zagirovaa@netcon.pro>
2022-02-27 21:14:22 +0300

    Added arguments parsing and annotations.

--- mp3rename.py
+++ mp3rename.py
@@ -1,41 +1,73 @@
+from __future__ import annotations
+import argparse
 import os
-import sys
 import glob
 import uuid
 
-MODES = ("byuuid", "bycode")
-TEMPLATE = "*.mp3"
-EXTENSION = ".mp3"
-    
 
-# Two parameters must be passed
-# - folder with files
-# - rename mode (byuuid, bycode)
-if len(sys.argv) > 2:
-    folder = sys.argv[1]
-    mode = sys.argv[2]
-    if os.path.isdir(folder) and mode in MODES:
-        list_of_files = glob.glob(folder + TEMPLATE)
+MODES: Tuple[str, str] = ("byuuid", "bycode")
+EXTENSION: str = ".mp3"
+TEMPLATE: str = "*{}".format(EXTENSION)
+DESCRIPTION: str = "The main task of this script is to rename mp3 files using random names."
+
+
+def get_args():
+    """
+    Returns the arguments passed to the application
+    """
+    parser: Object = argparse.ArgumentParser(description = DESCRIPTION)
+    parser.add_argument('folder', 
+                        metavar = "FOLDER", 
+                        type = str, 
+                        nargs = "?", 
+                        default = os.getcwd(), 
+                        help = "Folder with mp3 files.")
+    parser.add_argument('mode', 
+                        metavar = "MODE", 
+                        type = str, 
+                        nargs = "?", 
+                        default = "byuuid", 
+                        help = "Renaming mode (byuuid, bycode).")
+    return parser.parse_args()
+
+
+def main():
+    """
+    Aplication entry point
+    """
+    args: List[str] = get_args()
+    # Folder name must end with a separator
+    if args.folder[-1] is not os.sep:
+        args.folder += os.sep
+    if os.path.isdir(args.folder) and args.mode in MODES:
+        list_of_files: List[str] = glob.glob(args.folder + TEMPLATE)
         # There have to be at least one file
         if list_of_files:
             # ByUUID
-            if mode == MODES[0]:
+            if args.mode == MODES[0]:
                 for old_file in list_of_files:
-                    new_name = folder + str(uuid.uuid4()) + EXTENSION
-                    os.rename(old_file, new_name)
-                    print("--->>> File {} was renamed to {}".format(old_file, new_name))
+                    new_name: str = args.folder + str(uuid.uuid4()) + EXTENSION
+                    try:
+                        os.rename(old_file, new_name)
+                        print("--->>> File {} has been renamed to {}".format(old_file, new_name))
+                    except:
+                        print("Could not rename file {} using {} mode.".format(old_file, args.mode))
             # ByCode
             else:
-                new_name = ""
+                new_name: str = ""
                 for old_file in list_of_files:
                     for symbol in os.path.splitext(os.path.basename(old_file))[0]:
                         new_name += str(ord(symbol))
                     new_name += EXTENSION
-                    os.rename(old_file, new_name)
-                    print("--->>> File {} was renamed to {}".format(old_file, new_name))
+                    try:
+                        os.rename(old_file, new_name)
+                        print("--->>> File {} has been renamed to {}".format(old_file, new_name))
+                    except:
+                        print("Could not rename file {} using {} mode.".format(old_file, args.mode))
         else:
-            print("No file found.")
+            print("Files not found.")
     else:
         print("Error in passed parameters.")
-else:
-    print("Pass all the necessary parameters.")
+
+if __name__ == "__main__":
+    main()
